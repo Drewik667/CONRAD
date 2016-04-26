24fe145ad1724b25a44cc333efb132ea704c7a10
Andrzej Liebert <gu74xyga@stud.informatik.uni-erlangen.de>
2016-04-26 10:25:05 +0200

    first commit

---
+++ src/edu/stanford/rsl/tutorial/gu74xyga/my_phantom.java
@@ -0,0 +1,17 @@
+package edu.stanford.rsl.tutorial.gu74xyga;
+
+import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+
+public class my_phantom extends Grid2D {
+
+	public my_phantom(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

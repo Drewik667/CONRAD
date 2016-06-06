b9c9c53a9cfaa6785d633bb2ac973d40777abc54
Andrzej Liebert <gu74xyga@stud.informatik.uni-erlangen.de>
2016-06-06 16:56:52 +0200

    RamP filter + backprojection start again

---
+++ src/edu/stanford/rsl/tutorial/gu74xyga/ParallelBackProjection.java
@@ -0,0 +1,20 @@
+package edu.stanford.rsl.tutorial.gu74xyga;
+
+import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+
+public class ParallelBackProjection {
+	
+	public ParallelBackProjection(Grid2D singoram){
+		
+	}
+	
+	public void RowWiseFilter(){
+		
+	}
+	
+	public static void main(String[] args) {
+	
+
+	}
+
+}
